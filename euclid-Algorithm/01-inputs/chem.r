a9502868a9fe233cf053586e14b163816cba35d9
Verse <micalcjr@gmail.com>
2014-04-01 21:00:46 -0500

    Critical point in development wanted a snapshot of working code before making some 'dangerous' changes
    Signed-off-by: Verse <micalcjr@gmail.com>

---
+++ euclid-Algorithm/01-inputs/chem.r
@@ -0,0 +1,7 @@
+ : : 0 in A1, A1p, B1, B1p, begin01
+ : : 0 DoneSignal
+begin01 1 :	b01func00GO : 1000
+b01func00GO 1 A1 1 B1 1 = B1pp 1 b01func00GO 1
+ : : 0
+ : : 0 BEGIN ABSENCE INDICATORS
+
---
+++ euclid-algorithm/02-stage-1/chem.r
@@ -0,0 +1,5 @@
+ : : 0 in A1, B1, begin01
+begin01 1	: b01func00GO 1 : 1000
+b01func00GO 1 A1 1 B1 1 	: B1pp 1 b01func00GO 1 : 1000
+ : : 0
+ : : 0 BEGIN ABSENCE INDICATORS
